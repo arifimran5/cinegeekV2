2e45f72aeb51377f628a6cd0458b8765ad2fcb84
arifimran5 <arifimran505@gmail.com>
2023-08-19 15:16:09 +0530

    theme ui change to switch

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -139,9 +139,7 @@ function Header() {
             </div>
           </div>
           <div className='hidden gap-6 md:flex md:items-center'>
-            <div className='p-[4px] bg-gray-100 rounded-md dark:bg-gray-700'>
-              <ThemeSwitcher />
-            </div>
+            <ThemeSwitcher />
             <Link href='/search'>
               {/* <input
               type='text'
--- components/Header/ThemeSwitcher.tsx
+++ components/Header/ThemeSwitcher.tsx
@@ -1,41 +1,41 @@
 'use client'
 
-import { Monitor, Moon, Sun, SunMoon } from 'lucide-react'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '../ui/DropDown'
+import { Monitor, Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { cn } from '@/utils/helper'
 
 export default function ThemeSwitcher() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   return (
-    <div className='cursor-pointer'>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <SunMoon size={20} />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem onClick={() => setTheme('light')} className='gap-2'>
-            <Sun size={20} />
-            <span className='text-base'>Light</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme('dark')} className='gap-2'>
-            <Moon size={20} />
-            <span className='text-base'>Dark</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            onClick={() => setTheme('system')}
-            className='gap-2'
-          >
-            <Monitor size={20} />
-            <span className='text-base'>System</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+    <div className='flex items-center gap-3 px-2 py-1 bg-gray-100 rounded-md dark:bg-gray-700'>
+      <button
+        className={cn(
+          theme === 'light' &&
+            'bg-accent p-1 rounded-md transition-colors duration-300 animate-in fade-in'
+        )}
+        onClick={() => setTheme('light')}
+      >
+        <Sun size={20} />
+      </button>
+      <button
+        className={cn(
+          theme === 'dark' &&
+            'bg-accent text-accent-foreground p-1 rounded-md transition-colors duration-300 animate-in fade-in'
+        )}
+        onClick={() => setTheme('dark')}
+      >
+        <Moon size={20} />
+      </button>
+      <button
+        className={cn(
+          theme === 'system' &&
+            'bg-accent p-1 rounded-md transition-colors duration-300 animate-in fade-in'
+        )}
+        onClick={() => setTheme('system')}
+      >
+        <Monitor size={20} />
+      </button>
     </div>
   )
 }
