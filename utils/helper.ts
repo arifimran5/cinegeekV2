b8986514e640b8b35b4ae5a7457f641af7ee5379
arifimran5 <arifimran505@gmail.com>
2023-08-26 19:21:28 +0530

    running-time func

--- utils/helper.ts
+++ utils/helper.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getRunningTime(time: number | undefined) {
+  if (!time) return "";
+  let hr = Math.floor(time / 60)
+  let min = time - (hr * 60)
+
+  return `${hr}hr ${min}m`
+}
